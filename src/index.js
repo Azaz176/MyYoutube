ed1905198fb352705c79b0bbd3dd5850f3261e69
Azaz <azaz.ali1706@gmail.com>
2024-06-28 10:26:30 +0530

    backend for myYoutube

---
+++ src/index.js
@@ -0,0 +1,92 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from "mongoose";
+import connectDB from "./db/db.index.js";
+
+
+
+
+connectDB()
+.then(()=>{
+    console.log("Connection successful")
+    app.on("error", (error)=>{
+        console.log("app cannot talk to db, but db is connected")
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`app is listening on port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONgo connection failed", err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DB is in another continent
+// use try catch and async await
+/*
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("app cannot talk to db, but db is connected")
+            throw error
+        })
+        app.listen(process.env.PORT, ()=>{
+            console.log(`app is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+})
+    */
